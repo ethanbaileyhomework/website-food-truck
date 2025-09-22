bdb7d2f34e1d8b131f42703a828610940d00f60b
ethanbaileyhomework <ethanbaileyhome@gmail.com>
2025-09-22 22:10:59 +1000

    Fix Netlify CMS admin route

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/admin",
+        destination: "/admin/index.html",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
