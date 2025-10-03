caba2785f4080fa439a6e5fc5d49612f31609259
ethanbaileyhomework <ethanbaileyhome@gmail.com>
2025-10-03 10:50:18 +1000

    Ensure OAuth env vars available at runtime

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
+const runtimeEnvKeys = [
+  "GITHUB_CLIENT_ID",
+  "GITHUB_CLIENT_SECRET",
+  "DECAP_GITHUB_CLIENT_ID",
+  "DECAP_GITHUB_CLIENT_SECRET",
+  "DECAP_GITHUB_REPO",
+  "DECAP_GITHUB_APP_ID",
+  "DECAP_GITHUB_API_ROOT",
+  "DECAP_BACKEND_NAME",
+  "DECAP_BACKEND_BRANCH",
+  "DECAP_OAUTH_BASE_URL",
+  "DECAP_OAUTH_ENDPOINT",
+  "DECAP_SITE_URL",
+  "DECAP_DISPLAY_URL",
+  "OAUTH_BASE_URL",
+];
+
+const runtimeEnv = runtimeEnvKeys.reduce((acc, key) => {
+  acc[key] = process.env[key];
+  return acc;
+}, {});
+
 const nextConfig = {
   output: "standalone",
+  experimental: {
+    runtimeEnv,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.ya?ml$/i,
